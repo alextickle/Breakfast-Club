5f05f64b5ee82e950e649a4c16287bad1add9514
Alex Tickle <alexander.tickle@gmail.com>
2017-08-19 11:30:40 -0700

    removed index.css import

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from "./App";
 import TestApp from './TestApp';
-import './index.css';
 
 ReactDOM.render(<TestApp />, document.getElementById('root'));
