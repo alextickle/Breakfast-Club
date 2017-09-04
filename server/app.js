af803e5ed605c7ab1e3076ac18ca4997e65dc5ef
Alex Tickle <alexander.tickle@gmail.com>
2017-09-03 22:17:58 -0700

    updated production server path

--- server/app.js
+++ server/app.js
@@ -31,12 +31,14 @@ app.use(
 	})
 );
 
-app.use(
-	'/graphiql',
-	graphiqlExpress({
-		endpointURL: '/graphql'
-	})
-);
+if (process.env.NODE_ENV !== 'development') {
+	app.use(
+		'/graphiql',
+		graphiqlExpress({
+			endpointURL: '/graphql'
+		})
+	);
+}
 
 app.get('*', function(request, response) {
 	response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
