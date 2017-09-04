0d9abaf0eb9291b1682b7d4fb7ed174e2131ba30
Alex Tickle <alexander.tickle@gmail.com>
2017-09-04 10:52:23 -0700

    removed createNetworkInterface

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,18 @@ import MainContainer from './containers/MainContainer';
 import initStore from './state/store';
 import PathConfig from './config/PathConfig';
 
-let store = initStore();
+const store = initStore();
 
 const networkInterface = createNetworkInterface({
 	uri: `${PathConfig.serverDomain()}graphql`
 });
 
-const client = new ApolloClient({
-	networkInterface: networkInterface
-});
+const client =
+	process.env.NODE_ENV === 'development'
+		? new ApolloClient({
+				networkInterface: networkInterface
+			})
+		: new ApolloClient();
 
 const App = () =>
 	<ApolloProvider store={store} client={client}>
