5ceb13a9efba60b34954e0e2adab6c46b21d9652
Alex Tickle <alexander.tickle@gmail.com>
2017-09-09 18:03:00 -0700

    fixed addMessageMutation

--- client/src/mutations/addMessageMutation.js
+++ client/src/mutations/addMessageMutation.js
@@ -1,10 +1,12 @@
 import { gql } from 'react-apollo';
 
 const addMessageMutation = gql`
-	mutation addMessage($content: String!, $user_id: String) {
+	mutation addMessage($content: String!, $user_id: String!) {
 		addMessage(content: $content, user_id: $user_id) {
+			id
 			content
 			user {
+				id
 				firstName
 				lastName
 			}
