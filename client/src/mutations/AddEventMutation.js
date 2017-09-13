8f02e063691943027c444c3d3c5679cdd81afc87
Alex Tickle <alexander.tickle@gmail.com>
2017-09-13 07:54:12 -0700

    rename addEventMutation

--- client/src/mutations/AddEventMutation.js
+++
@@ -1,9 +0,0 @@
-import { gql } from 'react-apollo';
-
-const addEventMutation = gql`
-	mutation addEvent {
-		addEvent
-	}
-`;
-
-export default addEventMutation;
--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,15 +1,10 @@
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import * as reducers from './ducks';
 
 export default function initStore() {
 	const rootReducer = combineReducers(reducers);
-	const middleware = applyMiddleware(thunk);
 	return createStore(
 		rootReducer,
-		compose(
-			middleware,
-			window.devToolsExtension ? window.devToolsExtension() : f => f
-		)
+		window.devToolsExtension ? window.devToolsExtension() : f => f
 	);
 }
