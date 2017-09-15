6ccbd6aa1ceb84096fbef00239ce28df3596e6f0
Alex Tickle <alexander.tickle@gmail.com>
2017-09-15 07:16:19 -0700

    fixed serverSync

--- client/src/components/MessageBoard.js
+++ client/src/components/MessageBoard.js
@@ -3,59 +3,43 @@ import Moment from 'react-moment';
 import moment from 'moment';
 
 const MessageBoard = props => {
-	const syncToServerTime = input_date => {
-		let iso = new Date(input_date).toISOString().split(".")[0];
-		return moment(iso, "YYYY-MM-DDTHH:mm:ss").format();
-	}
+  let mapped = props.messages.map((message, i) => (
+    <div className="individual-message" key={i}>
+      <div className="sender">{props.user.firstName}</div>
+      <div className="time-stamp">
+        <Moment fromNow>{moment(message.createdAt).format()}</Moment>
+      </div>
+      <div className="message-content">{message.content}</div>
+    </div>
+  ));
 
-	let mapped = props.messages.map(function(message, i) {
-		let timeStamp = message.createdAt;
-		return (
-			<div className="individual-message" key={i}>
-				<div className="sender">
-					{props.user.firstName}
-				</div>
-				<div className="time-stamp">
-					<Moment fromNow>
-						{syncToServerTime(timeStamp)}
-					</Moment>
-				</div>
-				<div className="message-content">
-					{message.content}
-				</div>
-			</div>
-		);
-	});
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.addMessage(this.input.value, props.user.id);
+    this.input.value = '';
+  };
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		props.addMessage(this.input.value, props.user.id);
-		this.input.value = '';
-	};
-
-	return (
-		<div className="message-board">
-			<div className="message-box">
-				{mapped.reverse()}
-			</div>
-			<form name="submitMessage" onSubmit={handleSubmit}>
-				<div>
-					<input
-						className="submit-field"
-						size="20"
-						type="text"
-						placeholder="type a message"
-						name="message"
-						autoComplete="off"
-						ref={input => (this.input = input)}
-					/>
-				</div>
-				<div>
-					<input className="submit-chat-button" type="submit" value="Send" />
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className="message-board">
+      <div className="message-box">{mapped.reverse()}</div>
+      <form name="submitMessage" onSubmit={handleSubmit}>
+        <div>
+          <input
+            className="submit-field"
+            size="20"
+            type="text"
+            placeholder="type a message"
+            name="message"
+            autoComplete="off"
+            ref={input => (this.input = input)}
+          />
+        </div>
+        <div>
+          <input className="submit-chat-button" type="submit" value="Send" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default MessageBoard;
